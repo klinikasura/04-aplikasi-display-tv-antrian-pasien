c30c581dfe35576aab0ac2b66229bd9349656578
Klinik Utama dr. Aisyah <94031971+klinikutamadraisyah12@users.noreply.github.com>
2022-01-21 08:16:49 +0700

    Add files via upload

---
+++ tv.php
@@ -0,0 +1,17 @@
+<link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" />
+<p></p>
+<center><button id="goFS">KLIK LAYAR PENUH</button></center>
+<p></p>
+
+
+<script>
+  var goFS = document.getElementById("goFS");
+  goFS.addEventListener("click", function() {
+      document.body.requestFullscreen();
+  }, false);
+</script>
+
+
+
+<iframe width="1000" height="100%" src="http://192.168.88.203/dashboard/KHANZALITE-PAKESUARA/anjungan/loket" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
